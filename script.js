254d567e1cde6e5ae4557123c1dcead1937b83ff
Yuri <4117755ad63@gmail.com>
2021-06-21 20:53:18 +0300

    add event

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
+let item;
 document.body.addEventListener("dragstart", function (event) {
   if (!event.target.closest(".item")) return;
+  item = event.target.closest(".item");
   event.target.classList.add("hold");
+  event.target.innerHTML = "Ещё чуток осталось";
+  setTimeout(() => event.target.classList.add("hidden"), 0);
 });
 
 document.body.addEventListener("dragend", function (event) {
   if (!event.target.closest(".item")) return;
-  event.target.classList.remove("hold");
-});
-
-document.body.addEventListener("dragstart", function (event) {
-  if (!event.target.closest(".placeholder")) return;
-  console.log("start");
-});
-
-document.body.addEventListener("dragend", function (event) {
-  if (!event.target.closest(".placeholder")) return;
-  console.log("end");
-  event.preventDefault();
+  event.target.innerHTML = "Перетащи меня";
+  event.target.className = "item";
 });
 
 document.body.addEventListener("dragover", function (event) {
   if (!event.target.closest(".placeholder")) return;
-  console.log("over");
+  event.preventDefault();
+});
+
+document.body.addEventListener("dragenter", function (event) {
+  if (!event.target.closest(".placeholder")) return;
+  event.target.closest(".placeholder").classList.add("hovered");
+});
+
+document.body.addEventListener("dragleave", function (event) {
+  if (!event.target.closest(".placeholder")) return;
+  event.target.closest(".placeholder").classList.remove("hovered");
 });
 
 document.body.addEventListener("drop", function (event) {
   if (!event.target.closest(".placeholder")) return;
-  console.log("drop");
+  event.target.closest(".placeholder").classList.remove("hovered");
+  event.target.append(item);
 });
